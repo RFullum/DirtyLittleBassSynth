ba45ad86c485bd73e8d79346519f367e5bd1b1ac
rfullum <rfullum@gmail.com>
2020-12-26 15:22:32 +0000

    SubOscillator.cpp
    Going through code and breaking the header files out to the proper .h/.cpp two-file layout: SubOscillator.h & SubOscillator.cpp

---
+++ Source/SubOscillator.cpp
@@ -0,0 +1,55 @@
+/*
+  ==============================================================================
+
+    SubOscillator.cpp
+    Created: 26 Dec 2020 3:16:45pm
+    Author:  Robert Fullum
+
+  ==============================================================================
+*/
+
+#include "SubOscillator.h"
+
+/// Sets sampleRate of sub oscillators
+void SubOsc::setSampleRate(float SR)
+{
+    sinSub.setSampleRate(SR);
+    squareSub.setSampleRate(SR);
+    sawSub.setSampleRate(SR);
+}
+
+/// Populates values to the sub oscillator wavetables
+void SubOsc::populateWavetable()
+{
+    sinSub.populateWavetable();
+    squareSub.populateWavetable();
+    sawSub.populateWavetable();
+}
+
+/**
+Sets playback frequency of sub oscillator scaled 0 to 2 octaves below
+noteFreq is the playback note frequency. denom is the denominator to chose sub octave: 1, 2, or 4
+*/
+void SubOsc::setIncrement(float noteFreq, int denom)
+{
+    float subFreq = noteFreq / (float)denom;
+    
+    sinSub.setIncrement(subFreq);
+    squareSub.setIncrement(subFreq);
+    sawSub.setIncrement(subFreq);
+}
+
+/**
+Playback function: gain arguments come from SubOscParamControl class member functions to morph between the three
+wave shapes: sinSubGain( ), squareSubGain( ), and sawSubGain( )
+*/
+float SubOsc::process(float sinGain, float squareGain, float sawGain)
+{
+    float sinVal = sinSub.process() * sinGain;
+    float squareVal = squareSub.process() * squareGain;
+    float sawVal = sawSub.process() * sawGain;
+    
+    float outVal = (sinVal + squareVal + sawVal) * 0.5f;
+    
+    return outVal;
+}
--- Source/SubOscillator.h
+++ Source/SubOscillator.h
@@ -17,49 +17,24 @@ class SubOsc
 {
 public:
     /// Sets sampleRate of sub oscillators
-    void setSampleRate(float SR)
-    {
-        sinSub.setSampleRate(SR);
-        squareSub.setSampleRate(SR);
-        sawSub.setSampleRate(SR);
-    }
+    void setSampleRate(float SR);
     
     /// Populates values to the sub oscillator wavetables
-    void populateWavetable()
-    {
-        sinSub.populateWavetable();
-        squareSub.populateWavetable();
-        sawSub.populateWavetable();
-    }
+    void populateWavetable();
+    
     
     /**
      Sets playback frequency of sub oscillator scaled 0 to 2 octaves below
      noteFreq is the playback note frequency. denom is the denominator to chose sub octave: 1, 2, or 4
      */
-    void setIncrement(float noteFreq, int denom)
-    {
-        float subFreq = noteFreq / (float)denom;
-        
-        sinSub.setIncrement(subFreq);
-        squareSub.setIncrement(subFreq);
-        sawSub.setIncrement(subFreq);
-    }
+    void setIncrement(float noteFreq, int denom);
     
-    /// Playback function
     /**
      Playback function: gain arguments come from SubOscParamControl class member functions to morph between the three
      wave shapes: sinSubGain( ), squareSubGain( ), and sawSubGain( )
      */
-    float process(float sinGain, float squareGain, float sawGain)
-    {
-        float sinVal = sinSub.process() * sinGain;
-        float squareVal = squareSub.process() * squareGain;
-        float sawVal = sawSub.process() * sawGain;
-        
-        float outVal = (sinVal + squareVal + sawVal) * 0.5f;
-        
-        return outVal;
-    }
+    float process(float sinGain, float squareGain, float sawGain);
+    
 private:
     // Wavetable instances
     Wavetable sinSub;
