0a10ee27173e76ebe9c848437f966238c35bb16b
RFullum <rfullum@gmail.com>
2026-04-27 12:12:28 -0400

    fix struct punctuation

--- Source/GUI/ColorPalette.h
+++ Source/GUI/ColorPalette.h
@@ -31,14 +31,14 @@ namespace Palette
 
     inline const Theme DefaultTheme
     {
-        juce::Colour(0xFF0D0E12);   // background:      not-quite-black
-        juce::Colour(0xFF1A1C24);   // structure:       dark blue-grey  - alt: 0xFF22252F
-        juce::Colour(0xFF00C8B4);   // primaryAccent:   teal            - alt: 0xFF1BD4C0
-        juce::Colour(0xFF4C6EF5);   // secondaryAccent: purplish        - alt: 0xFF5B7BFF
-        juce::Colour(0xFFFF2D78);   // pinkAccent:      hot pink
-        juce::Colour(0xFFFF6B00);   // orangeAccent:    blaze orange
-        juce::Colour(0xFFD0D8E8);   // textPrimary:     blue-tinted off-white
-        juce::Colour(0xFF5A6480);   // textSecondary:   blue-gray
+        juce::Colour(0xFF0D0E12),   // background:      not-quite-black
+        juce::Colour(0xFF1A1C24),   // structure:       dark blue-grey  - alt: 0xFF22252F
+        juce::Colour(0xFF00C8B4),   // primaryAccent:   teal            - alt: 0xFF1BD4C0
+        juce::Colour(0xFF4C6EF5),   // secondaryAccent: purplish        - alt: 0xFF5B7BFF
+        juce::Colour(0xFFFF2D78),   // pinkAccent:      hot pink
+        juce::Colour(0xFFFF6B00),   // orangeAccent:    blaze orange
+        juce::Colour(0xFFD0D8E8),   // textPrimary:     blue-tinted off-white
+        juce::Colour(0xFF5A6480)    // textSecondary:   blue-gray
     };
 
 }   // namespace Palette
