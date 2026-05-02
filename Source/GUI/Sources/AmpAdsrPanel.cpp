0cfc4e4ba4dfe41c6bab5deeb883d05b714bfa3e
RFullum <rfullum@gmail.com>
2026-05-02 16:44:56 -0400

    tweak layout

--- Source/GUI/Sources/AmpAdsrPanel.cpp
+++ Source/GUI/Sources/AmpAdsrPanel.cpp
@@ -62,13 +62,15 @@ void AmpAdsrPanel::resized()
     sectionLabel.setBounds(bounds.removeFromTop(bounds.proportionOfHeight(0.08f)));
     
     const int margin = bounds.proportionOfWidth(0.15f);
-    adsrVisual.setBounds(bounds.removeFromTop(bounds.proportionOfHeight(0.5f)).reduced(margin, 0));
+    adsrVisual.setBounds(bounds.removeFromTop(bounds.proportionOfHeight(0.5f))
+                               .reduced(margin, 0));
     
     const int rowH = bounds.proportionOfHeight(0.25f);
-    auto adsrRow = [&](juce::Slider &slider, juce::Label &label)
+    auto adsrRow   = [&](juce::Slider &slider, juce::Label &label)
     {
         auto row = bounds.removeFromTop(rowH);
-        label.setBounds(row.removeFromLeft(margin));
+        label .setBounds(row.removeFromLeft(row.proportionOfWidth(0.1375f)));
+        row   .removeFromRight(18);
         slider.setBounds(row);
     };
 
